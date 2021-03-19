584f9953b5ab634deadf86837388ebd132e3d69b
Stefan Cvetkovic <stefan_cvetkovic@outlook.com>
2021-03-19 15:35:30 +0100

    Add project files.

---
+++ TablesReach/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TablesReach.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+}
